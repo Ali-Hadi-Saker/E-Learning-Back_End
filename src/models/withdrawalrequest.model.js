b83db1d8e253f71c269844cac7c77c1bcd4bad6f
Ali-Hadi-Saker <alihady530@gmail.com>
2024-08-04 15:43:58 +0300

    adding withdraw model

---
+++ src/models/withdrawalrequest.model.js
@@ -0,0 +1,25 @@
+import mongoose, {Schema} from "mongoose";
+
+
+const withdrawalSchema = new Schema({
+    students: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    class: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Class',
+        required: true
+    },
+    reason: {
+        type: String,
+        required: true
+    },
+    status: {
+        type: String,
+        enum: ['Pending', 'Rejected', 'Approved']
+    }
+})
+
+export const WithdrawlRequest = mongoose.model('WithdrawlRequest', withdrawalSchema)
