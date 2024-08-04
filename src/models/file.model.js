f6833fa9019c1d6fe8cc8a0d8caff5b6639dd645
Ali-Hadi-Saker <alihady530@gmail.com>
2024-08-04 15:32:30 +0300

    adding file class

---
+++ src/models/file.model.js
@@ -0,0 +1,26 @@
+import mongoose, {Schema} from "mongoose"
+
+
+const fileSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+      },
+      path: {
+        type: String,
+        required: true,
+      },
+      type: {
+        type: String,
+        enum: ['pdf', 'doc', 'ppt', 'other'],
+        required: true,
+      },
+      class: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Class',
+        required: true,
+      },
+    });
+
+export const File = mongoose.model('File', fileSchema)
