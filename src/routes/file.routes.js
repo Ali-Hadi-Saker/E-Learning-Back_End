8a77ec89b6cde21e5a1c4b69c606fdb3187a3c46
Ali-Hadi-Saker <alihady530@gmail.com>
2024-08-06 11:40:21 +0300

    adding middleware

--- src/routes/file.routes.js
+++ src/routes/file.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { uploadFile } from "../controllers/fileUpload.controller.js";
 import { downloadFile } from "../controllers/fileDownload.controller.js";
+import { authMiddleware } from "../middleware/auth.js";
 
 const router = new Router()
 
-router.post('/upload', uploadFile)
-router.get('/:classId/download/:filename', downloadFile)
+router.post('/upload',authMiddleware, uploadFile)
+router.get('/:classId/download/:filename',authMiddleware, downloadFile)
 
 export default router
