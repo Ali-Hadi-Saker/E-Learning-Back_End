41b2aee0547b6061b58cdb9cae5b4166f82c7228
Ali-Hadi-Saker <alihady530@gmail.com>
2024-08-06 20:30:33 +0300

    adding admin middleware for uploading file and creating class

--- src/routes/classes.routes.js
+++ src/routes/classes.routes.js
@@ -2,10 +2,11 @@ import { Router } from "express";
 import { createClass, getAllClasses } from "../controllers/class.controller.js";
 import { authMiddleware } from "../middleware/auth.js";
 import { enrollClass } from "../controllers/enrollClass.controller.js";
+import { adminMiddleware } from "../middleware/adminMiddleware.js";
 
 const router = new Router()
 
-router.post('/create', createClass)
+router.post('/create',authMiddleware, adminMiddleware, createClass)
 router.post('/:classId/enroll', authMiddleware, enrollClass)
 router.get('/', getAllClasses)
 
--- src/routes/file.routes.js
+++ src/routes/file.routes.js
@@ -2,10 +2,11 @@ import { Router } from "express";
 import { uploadFile } from "../controllers/fileUpload.controller.js";
 import { downloadFile } from "../controllers/fileDownload.controller.js";
 import { authMiddleware } from "../middleware/auth.js";
+import { adminMiddleware } from "../middleware/adminMiddleware.js";
 
 const router = new Router()
 
-router.post('/upload',authMiddleware, uploadFile)
+router.post('/upload',authMiddleware,adminMiddleware, uploadFile)
 router.get('/:classId/download/:filename',authMiddleware, downloadFile)
 
 export default router
