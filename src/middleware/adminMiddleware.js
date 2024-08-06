5da67affad37517aaaffe0599e10897be206959c
Ali-Hadi-Saker <alihady530@gmail.com>
2024-08-06 20:20:54 +0300

    creating admin middleware

---
+++ src/middleware/adminMiddleware.js
@@ -0,0 +1,14 @@
+export const adminMiddleware = (req, res, next) => {
+    try {
+        if (req.user && req.user.role === 'admin') {
+
+            next();
+
+        } else {
+
+            return res.status(403).json({ message: 'Access denied: Admins only.' });
+        }
+    } catch (error) {
+        return res.status(500).json({ message: 'Server error: ' + error.message });
+    }
+};
